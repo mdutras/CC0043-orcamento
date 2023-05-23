a34186a171338ebc94488b42454309fac3e40c8b
GilvanG <gilvango.j@gmail.com>
2023-05-22 21:28:16 -0300

    (feature): Navigation

--- components/List/index.tsx
+++ components/List/index.tsx
@@ -1,29 +1,51 @@
-import { StyleSheet, Text, View } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 import { Button, List, MD3Colors } from "react-native-paper";
 import { colors } from "../../styles/colors";
+import { Container } from "../Container";
 
-export const ListGraphics = () => (
+export const ListGraphics = ({navigation}: NativeStackScreenProps<RootStackParamList, 'Orçamentos'>) => (
+  <Container>
   <List.Section style={styles.section}>
     <Button
       style={styles.item}
-      icon="camera"
+      icon="chart-line"
       mode="outlined"
-      onPress={() => console.log("Pressed")}
+      onPress={() => navigation.navigate('Grafico1')}
     >
       Press me
     </Button>
-    <List.Item
-      title="Second Item"
-      left={() => <List.Icon color={MD3Colors.tertiary70} icon="folder" />}
-    />
+
+    <Button
+      style={styles.item}
+      icon="chart-areaspline"
+      mode="outlined"
+      onPress={() => navigation.navigate('Grafico1')}
+    >
+      Press me2
+    </Button>
+
+    <Button
+      style={styles.item}
+      icon="chart-arc"
+      mode="outlined"
+      onPress={() => navigation.navigate('Grafico1')}
+    >
+      Press me2
+    </Button>
   </List.Section>
+  </Container>
 );
 
 const styles = StyleSheet.create({
   section: {
     padding: 10,
+    height: Dimensions.get("screen").height * 0.55,
+    display: "flex",
+    justifyContent: 'center',
   },
   item: {
+    marginVertical: 10,
     // borderRadius: 10,
     borderColor: colors.green,
     // borderWidth: 1
---
+++ pages/Grafico1/index.tsx
@@ -0,0 +1,46 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { Text } from "react-native-paper";
+import { View } from "react-native";
+import { Container } from "../../components/Container";
+
+export const Grafico1 = ({navigation}: NativeStackScreenProps<RootStackParamList, 'Grafico1'>) => (
+    <Container>
+        <Text>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet reiciendis sunt quis, ab nesciunt voluptatum voluptatem quo repudiandae modi, possimus harum rerum, dicta nulla veniam dolores. Ullam culpa alias ratione.
+        </Text>
+    </Container>
+)
